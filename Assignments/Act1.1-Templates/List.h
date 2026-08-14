84103678664854e54ee62889b4c8058c98f979d6
PamelaHc <pamehc11@gmail.com>
2026-08-14 16:52:31 -0600

    cambio

---
+++ Assignments/Act1.1-Templates/List.h
@@ -0,0 +1,32 @@
+//Creamos clase que se llame list para guardar cualquier tipo de elementos
+
+#ifndef List_h //si la funcion no esta definida la defina
+#define List_h
+#include <vector>
+
+
+template <typename T>  //esta clase puede utilizar templates
+class List {
+private:
+    vector<T> list;
+    int size;
+public:
+    List();
+    void insert(T data);
+};
+
+template <typename T>
+List<T>::List() { //el constructor pertenece a tal clase
+    size = 0;
+}
+
+template <typename T>
+void List<T>::insert(T data) {
+    list.push_back(data);
+    size++;
+}
+
+
+#endif /* List_h */
+
+//comando ndef 
--- Assignments/Act1.1-Templates/act1.1-Templates.cpp
+++ Assignments/Act1.1-Templates/act1.1-Templates.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
+#include "List.h"
+
+/* int sum(int a, int b){
+    return a+b;
+}
+
+int sum(double a, double b){
+    return a+b;
+}
+
+string sum(string a, string b){
+    return a + b;
+}  */
+
+template<typename T>
+T sum(T a, T b){
+    return a + b;
+    
+}
+
 int main(){
+
+    string a="hola ";
+    string b= "crayola";
+    cout<< "Sum of " << a << " and "<< b << " is: "<< sum(a, b) << endl;
+   
+    int c=5;
+    int d=10;
+
+    List<int> list;
 
     return 0;
 }
